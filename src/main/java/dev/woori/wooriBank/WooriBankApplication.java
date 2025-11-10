7721bb018705d2cd2735a612ee70cc0e2824330b
kimhm0828 <kimhm0828@gmail.com>
2025-11-10 11:53:30 +0900

    initial commit

---
+++ src/main/java/dev/woori/wooriBank/WooriBankApplication.java
@@ -0,0 +1,13 @@
+package dev.woori.wooriBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooriBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WooriBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/woori/wooriBank/WooriBankApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.woori.wooriBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooriBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
